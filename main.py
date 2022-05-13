d25018ce725f9d9c4ebb19961211d0ca56a15f3d
Dr-Banana <zhangxuanzhi10419@gmail.com>
2022-05-13 07:30:45 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,59 @@
+import cv2
+import numpy as np
+from knn import question
+import tensorpack
+
+cap = cv2.VideoCapture(0)
+imgTarget = cv2.imread('Capture.jpg')
+imgVideo = cv2.imread('Capture.jpg')
+
+# success, imgVideo = myVid.read()
+hT, wT, cT = imgTarget.shape
+imgVideo = cv2.resize(imgVideo, (wT, hT))
+
+orb = cv2.ORB_create(nfeatures=2000)
+kp1, des1 = orb.detectAndCompute(imgTarget, None)
+# imgTarget = cv2.drawKeypoints(imgTarget, kp1, None)
+
+while True:
+    success, imgWebcam = cap.read()
+    imgAug = imgWebcam.copy()
+    kp2, des2 = orb.detectAndCompute(imgWebcam, None)
+    # imgWebcam = cv2.drawKeypoints(imgWebcam, kp2, None)
+
+    bf = cv2.BFMatcher()
+    # sample code view https://docs.opencv.org/4.x/dc/dc3/tutorial_py_matcher.html
+    matches = bf.knnMatch(des1, des2, k=2)
+    good = []
+    for m, n in matches:
+        if m.distance < 0.75 * n.distance:
+            good.append(m)
+    print(len(good))
+    imgFeatures = cv2.drawMatches(imgTarget, kp1, imgWebcam, kp2, good, None,
+                                  flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+
+    if len(good) > 30:
+        srcPts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
+        dstPts = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
+        matrix, mask = cv2.findHomography(srcPts, dstPts, cv2.RANSAC, 5)
+
+        pts = np.float32([[0, 0], [0, hT], [wT, hT], [wT, 0]]).reshape(-1, 1, 2)
+        dst = cv2.perspectiveTransform(pts, matrix)
+        img2 = cv2.polylines(imgWebcam, [np.int32(dst)], True, [255, 0, 255], 3)
+
+        imgWarp = cv2.warpPerspective(imgVideo, matrix, (imgWebcam.shape[1], imgWebcam.shape[0]))
+
+        maskNew = np.zeros((imgWebcam.shape[0], imgWebcam.shape[1]), np.uint8)
+        cv2.fillPoly(maskNew, [np.int32(dst)], (255, 255, 255))
+        maskInv = cv2.bitwise_not(maskNew)
+        imgAug = cv2.bitwise_and(imgAug, imgAug, mask=maskInv)
+        imgAug = cv2.bitwise_or(imgWarp, imgAug)
+
+        cv2.imshow('imgAug', imgAug)
+    # cv2.imshow('imgWarp', imgWarp)
+    # cv2.imshow('img2', img2)
+    cv2.imshow('imgFeatures', imgFeatures)
+    # cv2.imshow('imgTarget', imgTarget)
+    # cv2.imshow('myVid', imgVideo)
+    # cv2.imshow('imgWebcam', imgWebcam)
+    cv2.waitKey(1)
